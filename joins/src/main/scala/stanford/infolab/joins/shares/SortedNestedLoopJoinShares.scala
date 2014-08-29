60d33abc27282e748ed53a76e15a8c3143fb6ea3
Firas Abuzaid <fabuzaid21@gmail.com>
2014-08-28 19:03:11 -0700

    Updated to Spark 1.0.2 and included fastutils library. Also added CompactBuffer, to use instead of ArrayBuffer (hasn't been swapped out yet).

--- joins/src/main/scala/stanford/infolab/joins/shares/SortedNestedLoopJoinShares.scala
+++ joins/src/main/scala/stanford/infolab/joins/shares/SortedNestedLoopJoinShares.scala
@@ -5,7 +5,7 @@ import stanford.infolab.joins.JoinsArguments
 class SortedNestedLoopJoin(joinsArgs: JoinsArguments) extends BaseShares(joinsArgs) {
 
   //TODO()    
-  def performLocalJoin(arrBuf: Seq[(Byte, Long, Long)]): Seq[Array[Long]] = {
+  override def performLocalJoin(arrBuf: Iterable[(Byte, Long, Long)]): Seq[Array[Long]] = {
     null
   }
 }
